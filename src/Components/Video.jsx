fdf29b8b78db113b6031107a9c826c865d0d159e
Divwardhan <divwardhan2404@gmail.com>
2025-06-16 14:34:05 +0530

    Added video

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import video from '../assets/sample.mp4'
 
 const Video = () => {
   return (
-    <div className="w-screen h-screen overflow-hidden">
-      <video 
-        className="w-full h-full object-cover object-center"
-        src={video}
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
-        Your browser does not support the video tag.
-      </video>
+    <div className="w-screen h-screen overflow-hidden relative">
+      <iframe
+        className="w-full h-full absolute top-0 left-0"
+        src="https://www.youtube.com/embed/4otRbkmtDm8?autoplay=1&mute=1&loop=1&playlist=4otRbkmtDm8&controls=0&modestbranding=1&showinfo=0&rel=0"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        allowFullScreen
+      ></iframe>
     </div>
   )
 }
